e1d70de4ff3a67485799ebd82c3bc2d1f5bdfeca
5PenguinsInATrenchCoat <draven2121@gmail.com>
2026-02-03 09:52:36 -0800

    Lesson 2-3

--- src/pokecache.test.ts
+++ src/pokecache.test.ts
@@ -1,16 +1,28 @@
-import { Cache } from "./pokecache";
+import { Cache } from "./pokecache.js";
 import { test, expect } from "vitest";
 
 test.concurrent.each([
     {
         key: "www.google.com",
         val: "Bulbasaur is cool",
-        interval: 5000,
+        interval: 4000,
     },
     {
         key: "nintendo.com",
         val: 12345,
         interval: 1000,
     }
-])(`Testing cache, ${interval} ms`)
+])("Test Caching $interval ms", async ({ key, val, interval }) => {
+    const cache = new Cache(interval);
+
+    cache.add(key, val);
+    const cached = cache.get(key);
+    expect(cached).toBe(val);
+
+    await new Promise((resolve) => setTimeout(resolve, interval + 100));
+    const reaped = cache.get(key);
+    expect(reaped).toBe(undefined);
+
+    cache.stopReapLoop();
+    });
 
